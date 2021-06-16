60d36fd0c5837020cbb3daaaf59b61dd07e2f402
Josh <joshua.foster@earthware.co.uk>
2021-06-16 20:18:36 +0100

    Minor tweaks

--- src/components/Layout/Navbar/NavigationBar.tsx
+++ src/components/Layout/Navbar/NavigationBar.tsx
@@ -5,20 +5,23 @@ import './NavigationBar.scss';
 
 function NavigationBar() {
   return (
-    <Navbar bg="light" className="navigation-bar">
+    <Navbar bg="light" expand="sm" className="navigation-bar">
       <Link to="/">
         <Navbar.Brand>
           <Header />
         </Navbar.Brand>
       </Link>
-      <Nav>
-        <NavLink className="navigation-bar__link" to="/">
-          Home
-        </NavLink>
-        <NavLink className="navigation-bar__link" to="/help">
-          Help
-        </NavLink>
-      </Nav>
+      <Navbar.Toggle aria-controls="navbar" />
+      <Navbar.Collapse id="navbar">
+        <Nav>
+          <NavLink className="navigation-bar__link" to="/">
+            Home
+          </NavLink>
+          <NavLink className="navigation-bar__link" to="/help">
+            Help
+          </NavLink>
+        </Nav>
+      </Navbar.Collapse>
     </Navbar>
   );
 }
